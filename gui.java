c5344578ad833bd1d93721b7a91ac1788d83a1db
lilwizzi9 <62917075+lilwizzi9@users.noreply.github.com>
2022-09-26 04:49:18 +0100

    Connected Gui to Conn Class

--- gui.java
+++ gui.java
@@ -15,24 +15,36 @@ public class gui extends Thread{
     private JButton connect;
     private JTextField ip;
 
+    private JButton sendMsg;
+    private JTextField msg;
+    private conn conn;
+
+
+
+    
+
     public gui(){
+        // Construct all our UI Components
         frame =new JFrame();
         util = new JPanel();
         chat = new JPanel();
         host = new JButton();
         connect = new JButton();
-        ip = new JTextField();
+        ip = new JTextField(16);
+        ip.setText("127.0.0.1");
+        sendMsg  = new JButton();
+        //sendMsg.setEnabled(false);
+        msg = new JTextField(16);
     }
 
 
 
     @Override
     public void run(){
-        frame.setMinimumSize(new Dimension(500,500));
+        frame.setMinimumSize(new Dimension(300,250));
         util.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         chat.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
-        chat.setLayout(new GridLayout(0,1));
 
         frame.add(chat, BorderLayout.CENTER);
         frame.add(util, BorderLayout.NORTH);
@@ -40,12 +52,18 @@ public class gui extends Thread{
 
         host.setText("Host");
         connect.setText("Connect");
+        sendMsg.setText("Send");
+
         util.add(host);
         util.add(connect);
+        util.add(ip);
+
+        chat.add(msg);
+        chat.add(sendMsg);
 
 
-
-
+        // Important For Buttons
+        init_button();
 
         // Finally
         frame.setDefaultCloseOperation(3);
@@ -56,5 +74,77 @@ public class gui extends Thread{
     }
 
     public String getIP(){return ip.getText();}
+
+    public void init_button(){
+        // Initalizes Events For all The Buttons
+
+        // host Button
+        host.addActionListener(new event() {
+
+            @Override
+            public void action() {
+                // init Conn Oject
+                conn = new conn(true,ip.getText()) {
+
+                    @Override
+                    public String rcv(String msg) {
+                        System.out.println(msg);
+                        return null;
+                    }
+                    
+                };
+                conn.start();
+            }
+            
+        });
+
+
+        // connect Button
+        connect.addActionListener(new event() {
+
+            @Override
+            public void action() {
+                // init Conn Oject
+                conn = new conn(false,"") {
+
+                    @Override
+                    public String rcv(String msg) {
+                        System.out.println(msg);
+                        return null;
+                    }
+                    
+                };
+                conn.start();
+
+            }
+            
+        });
+
+        // SendMsg Button
+        sendMsg.addActionListener(new event() {
+
+            @Override
+            public void action() {
+                conn.send(msg.getText());
+                
+            }
+            
+        });
+
+
+
+    }
+
+}
+
+
+abstract class event implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        action();
+        
+    }
+
+    public abstract void action();
 
 }
